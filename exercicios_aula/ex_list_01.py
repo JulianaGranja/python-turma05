93749265c62516c470eaf40d7f25ca122db42cb9
Teo Calvo <teocalvo2@gmail.com>
2025-03-12 22:56:48 -0300

    feat: aula 04 listas

---
+++ exercicios_aula/ex_list_01.py
@@ -0,0 +1,20 @@
+# %%
+
+dados = [
+    "Teo",
+    "Calvo",
+    32,
+    ["jr", "pl", "sr", "head", "pl"],
+    ["tapps", "sas", "via", "gc", "globo"],
+    [3000, 4500, 7600, 12000, 4500],
+]
+
+# Qual o nome e sobre nome dessa pessoa?
+
+# Qual a primeira empresa que ela trabalhoU?
+
+# Qual as duas últimas posiçoes no mercado (senioridade)?
+
+# Mostre os saltos de 2 em 2 empresas dessa pessoa.
+
+# Mostre o salário do último para o primeiro
