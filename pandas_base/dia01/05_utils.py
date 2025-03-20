47528a7d19177cddfce442b0e7130af02fa30610
Teo Calvo <teocalvo2@gmail.com>
2025-03-20 01:00:31 -0300

    feat: aula 01 pandas

--- pandas_base/dia01/05_utils.py
+++ pandas_base/dia01/05_utils.py
@@ -3,3 +3,19 @@ import pandas as pd
 
 df = pd.read_csv("ufs.csv", sep=";")
 df
+
+# %%
+df.head(2)
+
+# %%
+df.tail(3)
+
+# %%
+df.sample(4)
+
+# %%
+df.info(memory_usage='deep')
+
+# %%
+
+df.dtypes
